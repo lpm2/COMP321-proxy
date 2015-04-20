b2f771c85b026bc89b8a99fac106915024ddad54
Leo <lpm2@rice.edu>
2015-04-19 20:41:15 -0500

    Added echoserver code to main

--- proxy/proxy.c
+++ proxy/proxy.c
@@ -22,15 +22,49 @@ int	parse_uri(char *uri, char *target_addr, char *path, int *port);
 int
 main(int argc, char **argv)
 {
+	int listenfd, connfd, port, error;
+	socklen_t clientlen;
+	struct sockaddr_in clientaddr;
+	char haddrp[INET_ADDRSTRLEN];
+	char host_name[NI_MAXHOST];
+	
+	if (argc != 2) {
+        	fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        	exit(0);
+	}
+	
+	port = atoi(argv[1]);
+	listenfd = Open_listenfd(port);
+	
+	while (1) {
+    		
+    		clientlen = sizeof(clientaddr);
+    		connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+
+    		/* determine the domain name and IP address of the client */
+		error = getnameinfo((struct sockaddr *)&clientaddr, sizeof(clientaddr), host_name, sizeof(host_name), NULL,0, 0);
+	
+		if (error != 0) {
+			fprintf(stderr, "ERROR: %s\n", gai_strerror(error));
+			Close(connfd);
+			continue;
+		}
+		inet_ntop(AF_INET, &clientaddr.sin_addr, haddrp, INET_ADDRSTRLEN);
+		printf("server connected to %s (%s)\n", host_name, haddrp);
+
+    		echo(connfd);
+    		Close(connfd);
+    	}
+    	exit(0);
 
 	/* Check the arguments. */
-	if (argc != 2) {
+	/*if (argc != 2) {
 		fprintf(stderr, "Usage: %s <port number>\n", argv[0]);
 		exit(0);
-	}
+	}*/
 
 	/* Return success. */
-	return (0);
+	//return (0);
 }
 
 /*
